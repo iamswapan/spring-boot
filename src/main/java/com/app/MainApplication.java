0571bfdd89239af2891952af0f62cf464133d087
Swapan Mishra <iamswapan@gmail.com>
2017-06-13 13:01:06 +0530

    Application setup and test run successfully

---
+++ src/main/java/com/app/MainApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by swapan on 13/6/17.
+ */
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("**************My Spring Boot Application is up and running*************");
+    }
+}
